29fcf2d1f377f0719a94a718625f7d9dd3794b6d
Muhammad Bin Zohaib <144327624+ProMBZ@users.noreply.github.com>
2024-07-29 14:34:52 +0500

    Add files via upload

---
+++ Login Sign up/script.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Add your login logic here
+    console.log('Login clicked');
+});
+
+document.getElementById('signupForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Add your signup logic here
+    console.log('Sign Up clicked');
+});
